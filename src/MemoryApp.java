e19920978edb7437b6740f079df882c7fc602d34
tina-e <53038141+tina-e@users.noreply.github.com>
2020-12-08 23:18:27 +0100

    Updated GraphicsApp lib to 1.4.7

--- src/MemoryApp.java
+++ src/MemoryApp.java
@@ -2,14 +2,13 @@ import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
 public class MemoryApp extends GraphicsApp {
+
     @Override
     public void initialize() {
-
     }
 
     @Override
     public void draw() {
-
     }
 
     public static void main(String[] args) {
